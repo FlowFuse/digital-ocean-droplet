56556a7d7fcc8e82bd979f412db1258d9d375063
Ben Hardill <ben@flowforge.com>
2023-05-12 12:53:57 +0100

    Remove leading v

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@
 
 # source .env
 export DIGITALOCEAN_API_TOKEN
+FF_VERSION=`echo $FF_VERSION | sed 's/v//'`
 export FF_VERSION
-export FF_DASH_VERSION=`echo $FF_VERSION | sed 's/\./-/g' | sed 's/v//`
+export FF_DASH_VERSION=`echo $FF_VERSION | sed 's/\./-/g'`
+echo $FF_VERSION $FF_DASH_VERSION
 packer build template.json
