b9dca10f7e0ea6d2b5f30978d26ce337f4b470b9
Ben Hardill <ben@flowforge.com>
2023-05-12 12:19:42 +0100

    Fix errors

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@
 # source .env
 export DIGITALOCEAN_API_TOKEN
 export FF_VERSION
-export FF_DASH_VERSION=`echo $FF_VERSION | sed 's/\./-/g'`
+export FF_DASH_VERSION=`echo $FF_VERSION | sed 's/\./-/g' | sed 's/v//`
 packer build template.json
