ffa692d611f66d95a3479892d689e29d9f87c348
Ben Hardill <ben@flowforge.com>
2024-02-19 10:38:54 +0000

    Use flowfuse containers
    part of FlowFuse/admin#212
    
    Use `flowfuse/*` containers over `flowforge/*`

--- scripts/015-flowforge.sh
+++ scripts/015-flowforge.sh
@@ -2,9 +2,9 @@
 
 mkdir -p /opt/flowforge
 curl -L https://github.com/flowforge/docker-compose/archive/refs/tags/v${application_version}.tar.gz | tar zx --one-top-level=/opt/flowforge --strip-components=1
-docker pull flowforge/node-red
-docker pull flowforge/forge-docker
-docker pull flowforge/file-server
+docker pull flowfuse/node-red
+docker pull flowfuse/forge-docker
+docker pull flowfuse/file-server
 docker pull iegomez/mosquitto-go-auth
 docker pull postgres:14
 docker pull nginxproxy/nginx-proxy
