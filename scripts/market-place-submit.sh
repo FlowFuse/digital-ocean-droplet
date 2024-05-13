3a24be9d26f6c1e4c6bb2e81b905be6d07db4227
Ben Hardill <ben@flowforge.com>
2024-05-13 11:18:41 +0100

    Add auto update script

---
+++ scripts/market-place-submit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+IMAGE_ID=$(jq '.builds[-1].artifact_id | split(":") | tonumber' manifest.json)
+# this should come from an app secret, this is new FlowFuse app id
+APP_ID=92052e1d7b34519f48b3b269
+
+curl -X PATCH -H "Content-Type: application/json" -H "Authorization: Bearer ${DIGITALOCEAN_API_TOKEN} \
+  -d "{\"reasonForUpdate\": \"new release\", \"version\": \"${FF_VERSION}\", \"imageId\": ${$IMAGE_ID}}" https://api.digitalocean.com/api/v1/vendor-portal/apps/${APP_ID}/versions/${APP_VERSION}
